fb993b1e8be9ec457de3f813db4fcbd888fab597
Yan <janbel7991@gmail.com>
2026-03-20 18:47:49 +0200

    Simple tests, django tags added

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def HomePageView(request):
-    return HttpResponse('Hello world')
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+                    
+# def HomePageView(request):
+#     return HttpResponse('Hello world')
